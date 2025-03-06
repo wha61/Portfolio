31c44001bdb82b6b48616532fbe65591fbfe7d09
Wenxiang He (Bruce) <wha61@sfu.ca>
2025-03-06 12:34:20 -0800

    Update quartz.config.ts

--- quartz.config.ts
+++ quartz.config.ts
@@ -87,4 +87,16 @@ const config: QuartzConfig = {
   },
 }
 
+export const extraHead = `
+  <!-- Google tag (gtag.js) -->
+  <script async src="https://www.googletagmanager.com/gtag/js?id=G-0CPGN06NB5"></script>
+  <script>
+    window.dataLayer = window.dataLayer || [];
+    function gtag(){dataLayer.push(arguments);}
+    gtag('js', new Date());
+
+    gtag('config', 'G-0CPGN06NB5');
+  </script>
+`;
+
 export default config
